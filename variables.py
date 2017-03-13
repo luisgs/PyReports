ba5ff170ee5ce6b5a19a235a8822966be5887bc2
Ubuntu <luisgs@MaquinaUbuntu.mistermapache.f9.internal.cloudapp.net>
2017-03-13 23:54:41 +0100

    adding variables.py file

---
+++ variables.py
@@ -0,0 +1,12 @@
+# This file contains all the necesary variables
+
+def init():
+	global emailFrom, password, SMTPServer, peopleInCc, peopleInBcc
+	emailFrom = ""
+	password = 1234
+	SMTPServer = "smtp.gmail.com:587"
+	peopleInCc = [""]
+	peopleInBcc = [""]
+
+# passwords
+
