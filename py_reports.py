e032f5cf50f7c0a579e757a2d8dbe65ed383993d
Ubuntu <luisgs@MaquinaUbuntu.mistermapache.f9.internal.cloudapp.net>
2017-03-14 21:55:59 +0100

    Reading CSV header propertly and searching for key to return index

--- py_reports.py
+++ py_reports.py
@@ -3,19 +3,19 @@ import sys
 import os
 import sendEmail
 
-# List_Cases = [ [ 'Luis' , [ '123456' , '789456' ] ] , [ 'Javi' , [ '123' ] ] ]
+# List_Cases = [ [ 'Luis' , 'email@email.com' , [ '123456' , '789456' ] ] ]
 List_Cases = []
 
 
-def consultantCases(Consultant, CaseNumber):
+def consultantCases(Consultant, ConsultantEmail, CaseNumber):
     "We will insert in Global_Cases[], consultant cases"
     " case [0] = Consultant"
     " case [1] = CaseNumber"
     for i in range(len(List_Cases)):
         if Consultant == List_Cases[i][0]:
-            List_Cases[i][1].append(CaseNumber)
+            List_Cases[i][2].append(CaseNumber)
             return
-    List_Cases.append([Consultant, [CaseNumber]])
+    List_Cases.append([Consultant, ConsultantEmail, [CaseNumber]])
 
 
 def doWeHaveAFile():
@@ -31,8 +31,8 @@ def doWeHaveAFile():
 
 def printListCases():
     for consultantInfo in List_Cases:
-        print(consultantInfo[0]+":")
-        for cases in consultantInfo[1]:
+        print(consultantInfo[0] + " (" + consultantInfo[1] + "):")
+        for cases in consultantInfo[2]:
             print("\t %i" % cases)
 
 
@@ -45,22 +45,25 @@ def main():
     doWeHaveAFile()
     try:
         with open(sys.argv[1], 'rt') as ReportCsv:
-            next(ReportCsv)     # We skip first line, it is trash
             reader = csv.reader(ReportCsv)
             data = list(reader)
-            # row_count = len(data)
+            caseIndex = data[0].index('Case Number')    # caseID
+            ownerIndex = data[0].index('Case Owner')    # caseOwner
+            ownerEmailIndex = data[0].index('Case Owner eMail')  # ownerEmail
     except (IOError):
-        sys.stderr.write("ERROR: File (%s) cannot be openned" % sys.argv[1])
+        sys.stderr.write("ERROR: File (%s) cannot be openned\n" % sys.argv[1])
+        sys.exit(1)
+    except ValueError:
+        sys.stderr.write("ERROR: Error CSV header value is missing\n")
         sys.exit(1)
     else:
-        for row in data:
-            if len(row):        # if we have data!
-                if List_Cases:  # Global list HAS data
-                    consultantCases(row[7], int(row[0]))
-                else:
-                    List_Cases.append([row[7], [int(row[0])]])
-            else:   # Rows comming after are trash
-                break
+        for row in data[1:-7]:  # Skip CSV header and last lines!
+            if List_Cases:  # Global list HAS data
+                consultantCases(row[ownerIndex], row[ownerEmailIndex],
+                                int(row[caseIndex]))
+            else:   # if list is empty
+                List_Cases.append([row[ownerIndex], row[ownerEmailIndex],
+                                   [int(row[caseIndex])]])
     finally:
         # Python closes files automatically but... what the hell
         ReportCsv.close()
