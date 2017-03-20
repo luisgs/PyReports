9ef5415baea7e8e409b5d598ffd774dfdb086d9e
Ubuntu <luisgs@MaquinaUbuntu.mistermapache.f9.internal.cloudapp.net>
2017-03-20 10:51:25 +0100

    CSV text format is ISO and not UTF. We have added new functions and print function is now in better shape

--- functions.py
+++ functions.py
@@ -2,5 +2,29 @@ def RequestorRoleIsPartner(requestorRole):
     return (requestorRole == 'Partner')
 
 
+def IsLocationPrimary(assetLocPrimary):
+    return int(assetLocPrimary)
+
+
 def foo(Email, Role):
     return (Email.find('@hpe.com') and (Role is not 'Non HPE Partner'))
+
+
+def assetLocationStatus(status):
+    return (status == 'Active')
+
+
+def locatorIDstart4(locID):
+    return (locID[0] == '4')
+
+
+def sourceIDstart1(accID):
+    return (accID[0] == '1')
+
+
+def countrySubLoc(submitter, location):
+    return (submitter == location)
+
+
+def emailReqContains(email):
+    return ("hpe.com.am" in email)
--- py_reports.py
+++ py_reports.py
@@ -2,6 +2,7 @@ import csv
 import sys
 import os
 import sendEmail
+import codecs
 import functions
 
 # ListOfErrors = ['Luis' , 'email@email.com' , {case1: ['Error1']}]
@@ -22,16 +23,21 @@ def doWeHaveAFile():
 def printListCases():
     for consultantInfo in ListOfErrors:
         print(consultantInfo[0] + " (" + consultantInfo[1] + "):")
-        print(consultantInfo[2])
+        for case in consultantInfo[2]:
+            print("\t"+str(case))
+            for Error in consultantInfo[2][case]:
+                print("\t\t"+str(Error))
 
 
 def emailToConsultant():
     for consultantReport in ListOfErrors:
-        sendEmail.emailToConsultant(consultantReport[0], consultantReport[1], consultantReport[2])
+        sendEmail.emailToConsultant(consultantReport[0],
+                                    consultantReport[1], consultantReport[2])
+        break
 
 
 def insertConsultCase(Name, Email, CaseNumber, ErrorCode):
-    for i in range(1,len(ListOfErrors)):
+    for i in range(1, len(ListOfErrors)):
         if (ListOfErrors[i][1] == Email):
             if CaseNumber in ListOfErrors[i][2]:
                 # if CaseID exist, I append this new error
@@ -47,13 +53,21 @@ def insertConsultCase(Name, Email, CaseNumber, ErrorCode):
 def main():
     doWeHaveAFile()
     try:
-        with open(sys.argv[1], 'rt') as ReportCsv:
+        with codecs.open(sys.argv[1], 'r', 'iso-8859-1') as ReportCsv:
             reader = csv.reader(ReportCsv)
             data = list(reader)
             # We read CSV header!
+            # Specific Index values for reports
+            if 'Asset Location: Primary' in data[0]:
+                print(sys.argv[1])
+                reportNumber = 1
+                asLocPrimary = data[0].index('Asset Location: Primary')
+            else:
+                reportNumber = 2
+            # Common Index Values for all types reports
+            ownerEmailIndex = data[0].index('Case Owner eMail')
             caseIndex = data[0].index('Case Number')    # caseID
-            ownerIndex = data[0].index('Case Owner')    # caseOwner
-            ownerEmailIndex = data[0].index('Case Owner eMail')  # ownerEmail
+            ownerIndex = data[0].index('Case Owner')
             reqRoleIndex = data[0].index('Requestor Role')  # Req Role
     except (IOError):
         sys.stderr.write("ERROR: File (%s) cannot be openned\n" % sys.argv[1])
@@ -62,22 +76,31 @@ def main():
         sys.stderr.write("ERROR: Error CSV header value is missing\n")
         sys.exit(1)
     else:
-        for row in data[1:-7]:  # Skip CSV header and last lines!
-            if functions.RequestorRoleIsPartner(row[reqRoleIndex]):
-                # print("Role is Partner %s" % row[caseIndex])
-                insertConsultCase(row[ownerIndex], row[ownerEmailIndex],
-                                  int(row[caseIndex]), 'RoleIsPart')
-            if functions.foo(row[ownerEmailIndex], row[reqRoleIndex]):
-                # print("foo %s" % row[caseIndex])
-                insertConsultCase(row[ownerIndex], row[ownerEmailIndex],
-                                  int(row[caseIndex]), 'foo')
+        if reportNumber == 1:
+            for row in data[1:-7]:  # Skip CSV header and last lines!
+                if functions.IsLocationPrimary(row[asLocPrimary]):
+                    insertConsultCase(row[ownerIndex], row[ownerEmailIndex],
+                                      int(row[caseIndex]), 'LocationNotPrimary')
+
+                if functions.emailReqContains(row[ownerEmailIndex]):
+                    insertConsultCase(row[ownerIndex], row[ownerEmailIndex],
+                                      int(row[caseIndex]), 'Requestor hpe.am')
+        elif reportNumber == 2:
+            for row in data[1:-7]:  # Skip CSV header and last lines!
+                if functions.RequestorRoleIsPartner(row[reqRoleIndex]):
+                    # print("Role is Partner %s" % row[caseIndex])
+                    insertConsultCase(row[ownerIndex], row[ownerEmailIndex],
+                                      int(row[caseIndex]), 'RoleIsPart')
+                if functions.foo(row[ownerEmailIndex], row[reqRoleIndex]):
+                    # print("foo %s" % row[caseIndex])
+                    insertConsultCase(row[ownerIndex], row[ownerEmailIndex],
+                                      int(row[caseIndex]), 'foo')
     finally:
         # Python closes files automatically but... what the hell
         ReportCsv.close()
-
     # print cases per Consultant!!
     printListCases()
-    emailToConsultant()
+#    emailToConsultant()
 
 
 if __name__ == "__main__":
