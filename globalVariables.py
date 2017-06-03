6c24ddf68ccba1d166ac21f2ba2b56c3da0ac606
Ubuntu <luisgs@MaquinaUbuntu.mistermapache.f9.internal.cloudapp.net>
2017-06-03 18:05:09 +0200

    Adding intelligence and use cases

---
+++ globalVariables.py
@@ -0,0 +1,14 @@
+ErrorsDefinition = {"LocationNotPrimary": "Case Location is NOT primary",
+                    "ReqEndHPE": "Requestor is HPE.",
+                    "RoleIsNotPartner": "Requestor Role needs to be " +
+                    "change to Partner",
+                    "LocStatusDisable": "The Asset Location field is " +
+                    "incorrect: either the ID is not primary, or it is " +
+                    "a \"customer\", or it is inactive.",
+                    "countrySubLoc": "Country Submitter and " +
+                    "Asset Country Location does NOT match",
+                    "BUisMissing": "BU is empty, complete it!",
+                    "PPIDmiss": "The Asset Location field is empty.",
+                    "ReqIsNotHPE": "Requestor Role needs to be HPE",
+                    "missOPPID": "Opportunity ID provided but not " +
+                    "fulfilled in the case."}
